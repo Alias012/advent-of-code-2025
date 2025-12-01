497dea6053416bae00703b492a200de5ed11dcfd
Andrew Lia <a.r.lia@student.utwente.nl>
2025-12-01 12:14:58 +0100

    Day 1, p1 + p2

--- src/main/java/Day01.java
+++ src/main/java/Day01.java
@@ -1,5 +1,65 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Day01 {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println(new Day01().crackLock("src/main/resources/Day01.txt"));
+        System.out.println(new Day01().crackSecureLock("src/main/resources/Day01.txt"));
+    }
+
+    private int crackLock(String filename) {
+        int lock = 50;
+        int count = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                char direction = line.charAt(0);
+                int rotations = Integer.parseInt(line.substring(1));
+                if (direction == 'L') {
+                    rotations = -rotations;
+                }
+                lock = (lock + rotations) % 100;
+                if (lock == 0) {
+                    count++;
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading input file...");
+        } catch (NumberFormatException e) {
+            System.err.println("Error in input line format...");
+        }
+        return count;
+    }
+
+    private int crackSecureLock(String filename) {
+        int lock = 50;
+        int count = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                char direction = line.charAt(0);
+                int rotations = Integer.parseInt(line.substring(1));
+
+                int overflow = rotations / 100;
+                count += overflow;
+
+                int leftover = rotations - (100 * overflow);
+                if (direction == 'L') {
+                    leftover = -leftover;
+                }
+
+                int newLock = lock + leftover;
+                if (newLock > 99 || (newLock < 0 && lock != 0)) {
+                    count++;
+                }
+                lock = Math.abs(newLock % 100);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading input file...");
+        } catch (NumberFormatException e) {
+            System.err.println("Error in input line format...");
+        }
+        return count;
     }
 }
