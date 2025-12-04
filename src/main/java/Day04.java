c2ba2bd81b11f064cda6897b77460b976e0dab59
Andrew Lia <a.r.lia@student.utwente.nl>
2025-12-04 12:54:59 +0100

    Day 4, p1 + p2

---
+++ src/main/java/Day04.java
@@ -0,0 +1,121 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Day04 {
+    public static void main(String[] args) {
+        System.out.println(new Day04().accessibleRolls("src/main/resources/Day04.txt"));
+        System.out.println(new Day04().recursivelyAccessibleRolls("src/main/resources/Day04.txt"));
+    }
+
+    private int accessibleRolls(String filename) {
+        ArrayList<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading input file...");
+        }
+
+        int count = 0;
+        int rowLength = lines.get(0).length();
+        int colLength = lines.size();
+        byte[] grid = new byte[rowLength * colLength];
+        countFreeRolls(lines, grid, rowLength, colLength);
+        for (int i = 0; i < grid.length; i++) {
+            if (grid[i] < 4) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private void countFreeRolls(ArrayList<String> lines, byte[] grid, int rowLength, int colLength) {
+        for (int i = 0; i < rowLength; i++) {
+            String line = lines.get(i);
+            int rowVal = i * rowLength;
+            for (int j = 0; j < colLength; j++) {
+                if (line.charAt(j) == '@') {
+                    boolean notLowerRow = i != 0;
+                    boolean notHigherRow = i != rowLength - 1;
+                    boolean notLeftCol = j != 0;
+                    boolean notRightCol = j != colLength - 1;
+
+                    if (notLowerRow) {
+                        grid[(rowVal - rowLength) + j]++;
+                        if (notRightCol) {
+                            grid[rowVal - rowLength + j + 1]++;
+                        }
+                        if (notLeftCol) {
+                            grid[(rowVal - rowLength) + (j - 1)]++;
+                        }
+                    }
+                    if (notRightCol) {
+                        grid[rowVal + j + 1]++;
+                    }
+                    if (notLeftCol) {
+                        grid[rowVal + (j - 1)]++;
+                    }
+                    if (notHigherRow) {
+                        grid[rowVal + rowLength + j]++;
+                        if (notRightCol) {
+                            grid[rowVal + rowLength + j + 1]++;
+                        }
+                        if (notLeftCol) {
+                            grid[rowVal + rowLength + j - 1]++;
+                        }
+                    }
+                } else {
+                    grid[rowVal + j] = 4;
+                }
+            }
+        }
+    }
+
+    private long recursivelyAccessibleRolls(String filename) {
+        ArrayList<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading input file...");
+        }
+
+        int rowLength = lines.get(0).length();
+        int colLength = lines.size();
+        byte[] grid = new byte[rowLength * colLength];
+        return recursiveCountFreeRolls(lines, grid, rowLength, colLength);
+    }
+
+    private int recursiveCountFreeRolls(ArrayList<String> lines, byte[] grid, int rowLength, int colLength) {
+        int oldCount = 0;
+        int newCount = 0;
+
+        while (true) {
+            countFreeRolls(lines, grid, rowLength, colLength);
+            for (int i = 0; i < grid.length; i++) {
+                if (grid[i] < 4) {
+                    newCount++;
+                    int row = i / rowLength;
+                    int col = i % rowLength;
+                    String line = lines.get(row);
+                    lines.set(row, line.substring(0, col) + '.' + line.substring(col + 1));
+                }
+            }
+            if (newCount == oldCount) {
+                break;
+            }
+            oldCount = newCount;
+            Arrays.fill(grid, (byte) 0);
+        }
+
+        return newCount;
+    }
+}
