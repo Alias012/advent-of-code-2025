70f61b3265f230e63eae9c22d59f6ccf8fdbe397
Andrew Lia <a.r.lia@student.utwente.nl>
2025-12-07 21:17:30 +0100

    Improve Day 6

--- src/main/java/Day06.java
+++ src/main/java/Day06.java
@@ -3,6 +3,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import util.ReadFile;
 
 public class Day06 {
@@ -22,86 +25,66 @@ public class Day06 {
             System.err.println("Error reading input file...");
         }
 
-        int colLength = lines.size();
-        String[] operatorRow = lines.remove(colLength - 1);
-
         int rowLength = lines.getFirst().length;
-        long count = 0;
+        int colLength = lines.size() - 1;
+        String[] operatorRow = lines.remove(colLength);
 
-        for (int i = 0; i < rowLength; i++) {
-            long[] row = new long[colLength - 1];
-            for (int j = 0; j < colLength - 1; j++) {
-                row[j] = Long.parseLong(lines.get(j)[i]);
-            }
+        // transpose the list then reduce each row with relevant operator
+        return IntStream.iterate(0, i -> i < rowLength, i -> i + 1)
+                        .mapToLong(i -> {
+                            LongStream row = IntStream.iterate(0, j -> j < colLength, j -> j + 1)
+                                                      .mapToLong(j -> Long.parseLong(lines.get(j)[i]));
 
-            count += switch (operatorRow[i]) {
-                case "*" -> Arrays.stream(row).reduce(1, (x, y) -> x * y);
-                case "+" -> Arrays.stream(row).sum();
-                default -> -1;
-            };
-        }
-
-        return count;
+                            switch (operatorRow[i]) {
+                                case "*" -> { return row.reduce(1, (x, y) -> x * y); }
+                                case "+" -> { return row.sum(); }
+                                default -> { return -1; }
+                            }
+                        })
+                        .sum();
     }
 
     private static long cephalopodMath(String filename) {
         ArrayList<String> lines = ReadFile.getAllLines(filename);
-        int colLength = lines.size();
-        String operatorRow = lines.remove(colLength - 1);
+        int colLength = lines.size() - 1;
+        String operatorRow = lines.remove(colLength);
+        ArrayList<String> operators = new ArrayList<>(List.of(operatorRow.split("\\s+")));
 
-        int colWidth = 1;
-        int totalWidth = 0;
-        ArrayList<String[]> transpose = new ArrayList<>();
-        ArrayList<String> operators = new ArrayList<>();
-        operators.add(String.valueOf(operatorRow.charAt(0)));
+        //use the distance between operators to find width of each column
+        //essentially split on the location of operator and save spaces
+        return Arrays.stream(operatorRow.split("\\S"))
+                     .mapToInt(whitespace -> whitespace.length() + 1)
+                     .skip(1)
+                     .mapToLong(width -> {
+                         //transpose and save spaces
+                         String[] row = new String[colLength];
+                         for (int j = 0; j < colLength; j++) {
+                             String line = lines.get(j);
+                             row[j] = line.substring(0, width);
+                             lines.set(j, line.substring(width));
+                         }
 
-        //uses the distance between operators to find width of each column
-        //essentially splits on the location of operator and saves spaces
-        for (int i = 1; i < operatorRow.length(); i++) {
-            char c = operatorRow.charAt(i);
-            if (c != ' ') {
-                operators.add(String.valueOf(c));
-                String[] row = new String[colLength - 1];
-                for (int j = 0; j < colLength - 1; j++) {
-                    row[j] = lines.get(j).substring(totalWidth, totalWidth + colWidth - 1);
-                }
-                totalWidth += colWidth;
-                colWidth = 0;
-                transpose.add(row);
-            }
-            colWidth++;
-        }
+                         //find column-wise values (~ transpose again)
+                         LongStream cols = IntStream.iterate(width - 1, i -> i != -1, i -> i - 1)
+                                                    .mapToLong(length -> {
+                                                        long newNum = 0;
+                                                        for (String num : row) {
+                                                            char c = num.charAt(length);
+                                                            if (c != ' ') {
+                                                                newNum = newNum * 10 + Character.getNumericValue(c);
+                                                            }
+                                                        }
+                                                        return newNum;
+                                                    })
+                                                   .filter(num -> num != 0);
 
-        //the last column
-        String[] row = new String[colLength - 1];
-        for (int j = 0; j < colLength - 1; j++) {
-            row[j] = lines.get(j).substring(totalWidth);
-        }
-        transpose.add(row);
-
-        return transpose.stream()
-                        .map(oldRow -> { //find the column-wise values
-                                    int length = oldRow[0].length();
-                                    ArrayList<Long> newRow = new ArrayList<>();
-                                    while (length-- != 0) {
-                                        long newNum = 0;
-                                        for (String num : oldRow) {
-                                            char c = num.charAt(length);
-                                            if (c != ' ') {
-                                                newNum = newNum * 10 + Character.getNumericValue(c);
-                                            }
-                                        }
-                                        if (newNum != 0) {
-                                            newRow.add(newNum);
-                                        }
-                                    }
-                                    return newRow;})
-                        .mapToLong(newRow -> {
-                                    switch (operators.removeFirst()) {
-                                        case "*" -> { return newRow.stream().reduce(1L, (x, y) -> x * y); }
-                                        case "+" -> { return newRow.stream().reduce(0L, Long::sum); }
-                                        default -> { return -1; }
-                                    }})
-                        .sum();
+                         //reduce the values depending on relevant operator
+                         switch (operators.removeFirst()) {
+                             case "*" -> { return cols.reduce(1L, (x, y) -> x * y); }
+                             case "+" -> { return cols.reduce(0L, Long::sum); }
+                             default -> { return -1; }
+                         }
+                     })
+                     .sum();
     }
 }
